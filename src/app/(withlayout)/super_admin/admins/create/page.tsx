b6c8fbd972e486a402f036158d7bf750cd36ffcb
maheenur13 <differentmahee13@gmail.com>
2023-10-04 02:23:23 +0600

    new changes

---
+++ src/app/(withlayout)/super_admin/admins/create/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateAdmin = () => {
+  return (
+    <div>
+      <h3>Create Admin</h3>
+    </div>
+  )
+}
+
+export default CreateAdmin
---
+++ src/app/(withlayout)/super_admin/departments/page.tsx
@@ -0,0 +1,8 @@
+
+const ManageDepartments = () => {
+  return (
+    <div>ManageDepartments</div>
+  )
+}
+
+export default ManageDepartments
