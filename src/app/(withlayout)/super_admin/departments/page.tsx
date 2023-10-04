d8f812dcb275c89bdf9503caec16dc556730a776
maheenur13 <differentmahee13@gmail.com>
2023-10-04 10:19:00 +0600

    more done

--- src/app/(withlayout)/super_admin/departments/page.tsx
+++ src/app/(withlayout)/super_admin/departments/page.tsx
@@ -1,8 +1,35 @@
+"use client";
 
-const ManageDepartments = () => {
+import BreadCrumbWrapper from "@/components/ui/BreadCrumb";
+import { getUserInfo } from "@/services/auth.service";
+import { Button, Col, Row } from "antd";
+import Link from "next/link";
+import React from "react";
+
+const ManageDepartment = () => {
+  const { role } = getUserInfo();
   return (
-    <div>ManageDepartments</div>
-  )
-}
+    <div>
+      <BreadCrumbWrapper
+        items={[
+          {
+            label: `${role}`,
+            link: `/${role}`,
+          },
+        ]}
+      />
+      <Row justify={"space-between"} align={"middle"}>
+        <Col>
+          <h2>Manage Department</h2>
+        </Col>
+        <Col>
+          <Link href="/super_admin/departments/create">
+            <Button type="primary">Create Department</Button>
+          </Link>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default ManageDepartments
+export default ManageDepartment;
