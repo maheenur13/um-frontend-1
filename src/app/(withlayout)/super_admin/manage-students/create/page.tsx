2b890d798caf625ae7662e83360467bfdee9826b
maheenur13 <differentmahee13@gmail.com>
2023-10-05 00:16:51 +0600

    done

--- src/app/(withlayout)/super_admin/manage-students/create/page.tsx
+++ src/app/(withlayout)/super_admin/manage-students/create/page.tsx
@@ -1,8 +1,24 @@
+import StepperForm from "@/components/StepperForm"
 
 const CreateStudent = () => {
+  const steps = [
+    {
+      title: 'First',
+      content: 'First-content',
+    },
+    {
+      title: 'Second',
+      content: 'Second-content',
+    },
+    {
+      title: 'Last',
+      content: 'Last-content',
+    },
+  ];
   return (
     <div>
       <h2>Create Student</h2>
+      <StepperForm steps={steps} />
     </div>
   )
 }
